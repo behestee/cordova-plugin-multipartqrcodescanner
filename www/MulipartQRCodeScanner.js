4b6218aea7ea68136099d0e3c37e4701dd0c4218
Behestee <behestee@gmail.com>
2017-01-16 03:16:16 +0600

    testing to fixing

--- www/MulipartQRCodeScanner.js
+++ www/MulipartQRCodeScanner.js
@@ -3,7 +3,13 @@ var exec = require('cordova/exec');
 var MulipartQRCodeScanner = function () {};
 
 MulipartQRCodeScanner.prototype.scan = function (arg0, success, error) {
-    exec(success, error, "MulipartQRCodeScanner", "scan", []);
+    exec(success, error, "MulipartQRCodeScanner", "scan", [{
+            preferFrontCamera : false, // iOS and Android
+            showFlipCameraButton : true, // iOS and Android
+            showTorchButton : true, // iOS and Android
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            disableAnimations : true // iOS
+        }]);
 };
 
 
